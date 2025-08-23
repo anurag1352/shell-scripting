49b775e7fcabe7e6acd4d1269cafe345634e9871
Anurag <anurag13520@gmail.com>
2025-08-23 16:58:18 +0530

    Shell-Script

---
+++ myscripts/03_variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Write a script to Store your name , city and age. Then print
+name="Anurag"
+city="Panchkula"
+age=20
+
+echo "My Name is $name i'm from $city. & I'm $age years old."
+
+# TRY TO VARIABLE VALUE.
+name="Anurag Sharma"
+echo "My Name is $name i'm from $city. & I'm $age years old."
