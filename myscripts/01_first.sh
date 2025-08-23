1e1931fabb4fbc19f2a5081a82a13877ec0a3978
Anurag <anurag13520@gmail.com>
2025-08-23 16:48:39 +0530

    Shell-Script

---
+++ myscripts/01_first.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# THIS IS MY FIRST SHELL SCRIPT.
+echo "Learning Shell Scripting for devops."
---
+++ myscripts/02_comments.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "THIS SCRIPT IS ON COMMENTS IN SHELL SCRIPT."
+
+echo "Single line comment. denoted by (#)"
+# THIS IS SINGLE LINE COMMENT.
+
+echo "Multiple line comment. syntax << commentName
+.....
+.....
+commentName"
+<< comment
+THIS 
+IS 
+MULTI-
+-LINE
+COMMENTS.
+comment
+
